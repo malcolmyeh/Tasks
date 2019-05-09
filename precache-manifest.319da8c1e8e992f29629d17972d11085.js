c1b117e0cda604069a69fca0e42b092e62b413ee
malcolmyeh <malcolmyeh@gmail.com>
2019-05-08 22:13:16 -0400

    Updates

---
+++ precache-manifest.319da8c1e8e992f29629d17972d11085.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90e6f1ee13a74c3058a44acb29346cfc",
+    "url": "/tasks/index.html"
+  },
+  {
+    "revision": "e62f13759effd1ed8477",
+    "url": "/tasks/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/tasks/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "e62f13759effd1ed8477",
+    "url": "/tasks/static/js/main.d16dc530.chunk.js"
+  },
+  {
+    "revision": "7d8089a231448516824e",
+    "url": "/tasks/static/js/runtime~main.b1405a7c.js"
+  }
+]);
