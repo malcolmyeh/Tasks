cbde0b2675da6d62fe4a5e3407d8f89c9a4d978d
malcolmyeh <malcolmyeh@gmail.com>
2019-05-08 22:46:24 -0400

    added FlipMove animation

--- src/Tasks.js
+++ src/Tasks.js
@@ -1,16 +1,20 @@
 import React from 'react'
+import FlipMove from "react-flip-move";
 
 const Tasks = ({tasks, deleteTask}) => {
    const taskList = tasks.length ? (
        tasks.map(task => {
            return (
+            <FlipMove>
                <div key={task.id} className = "center">
                    <span onClick={() => {deleteTask(task.id)}}>{task.content}</span>
                </div>
-           )
+            </FlipMove>)
        })
    ) : (
-       <p className="center">No tasks.</p>
+        <FlipMove>
+            <p className="center">No tasks.</p>
+        </FlipMove>
    )
    return (
        <div className = "task list">
